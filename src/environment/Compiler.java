111d787ed2fe6928373b3685387e894374acd0c2
Luca-Laptop <1610927@stud.hs-mannheim.de>
2017-04-20 11:01:14 +0200

    created the Compiler

---
+++ src/environment/Compiler.java
@@ -0,0 +1,57 @@
+package environment;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Compiler {
+	private String[][] assemblerToMachine = new String[20][2];
+	private String[] assemblerText = new String[4096];
+	private short[] machineCode= new short[4096];
+
+	public void compile(String filename) {
+		try {
+			read(filename);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		assemblerToMachine();
+		write(filename);
+	}
+
+	public void read(String filename) throws IOException {
+		FileReader fileReader = new FileReader(filename);
+		BufferedReader bufferedReader = new BufferedReader(fileReader);
+		List<String> lines = new ArrayList<String>();
+		String line = null;
+		while ((line = bufferedReader.readLine()) != null) {
+			lines.add(line);
+		}
+		bufferedReader.close();
+		assemblerText = lines.toArray(new String[lines.size()]);
+	}
+
+	public void assemblerToMachine() {
+		for (int i = 0; i < assemblerText.length; i++) {
+			String[] lineInformation = new String[5];
+			String line = assemblerText[i].substring(0, assemblerText[i].indexOf(";"));
+			lineInformation = line.split(" ");
+
+			short machinecode = 0;
+			for (int j = 0; j < line.length(); j++) {
+				short value =Short.parseShort(assemblerToMachine[i]);
+				machinecode= (short) (machinecode^ value);
+
+			}
+
+		}
+
+	}
+
+	public void write(String filename) {
+
+	}
+
+}
